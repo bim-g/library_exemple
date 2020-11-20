5edb2ea8502bf5d7ff52cce51473c671517b2cf4
bim-g <ibmussafb@gmail.com>
2020-11-20 23:09:51 +0200

    initialisation du fichier composer pour gerer les autoloding

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-
-// use Wepesi\Core\App\Route\Router;
-
-use Wepesi\app\Demo;
-
-
 require_once 'config/init.php';
-    require_once 'route/index.php';
-    // $d = new Demo();
-    // echo $d->get();
-   
+    require_once 'route/index.php';   
 ?>

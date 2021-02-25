f10ae616fc15e96c38a0420ca3b306ef2cff9a19
bim-g <ibmussafb@gmail.com>
2021-02-25 21:50:28 +0200

    fix check file extension of a file

--- class/app/Controller.php
+++ class/app/Controller.php
@@ -3,14 +3,15 @@
     namespace Wepesi\App\Core;
     class Controller{
         
-        static function useModel($file)        {
+        static function useModel($file){
             if (is_file(ROOT . 'corp/' . $file . ".php")) {
                 require_once(ROOT . 'corp/' . $file . '.php');
             }
         }
-        static function useController($file)        {
-            if (is_file(ROOT . 'controller/' . $file . ".php")) {
-                require_once(ROOT . 'controller/' . $file . '.php');
+        static function useController($fileName){
+            $file=check_file_extention($fileName);
+            if (is_file(ROOT . 'controller/' . $file )) {
+                require_once(ROOT . 'controller/' . $file);
             }
         }       
     }

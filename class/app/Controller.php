c939b32de4c8e52eea6ebec4484c03eba1cdf597
bim-g <ibmussafb@gmail.com>
2021-02-25 21:58:41 +0200

    make controoler to be dynamic with sub-folder

--- class/app/Controller.php
+++ class/app/Controller.php
@@ -1,17 +1,23 @@
 <?php
 
     namespace Wepesi\App\Core;
-    class Controller{
-        
-        static function useModel($file){
-            if (is_file(ROOT . 'corp/' . $file . ".php")) {
-                require_once(ROOT . 'corp/' . $file . '.php');
+    class Controller{        
+        private static function useModel($fileName){
+            $directories = get_Sub_Directories("corp");
+            foreach($directories as $dir){
+                $file=ROOT.$dir."/".check_file_extention($fileName);
+                if (is_file($file )) {
+                    require_once($file);
+                }
             }
         }
         static function useController($fileName){
-            $file=check_file_extention($fileName);
-            if (is_file(ROOT . 'controller/' . $file )) {
-                require_once(ROOT . 'controller/' . $file);
+            $directories=get_Sub_Directories("controller");
+            foreach($directories as $dir){
+                $file=ROOT.$dir."/".check_file_extention($fileName);
+                if (is_file($file)) {
+                    require_once($file);
+                }
             }
         }       
     }
